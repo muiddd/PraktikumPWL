db362f2ecc4bce9c007f261be6fd00a95e9c0fa3
muid <muidabdulmuid99@gmail.com>
2026-04-12 22:33:01 +0700

    7.1

---
+++ app/Filament/Resources/Products/Tables/ProductsTable.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Filament\Resources\Products\Tables;
+
+use Filament\Actions\BulkActionGroup;
+use Filament\Actions\DeleteBulkAction;
+use Filament\Actions\EditAction;
+use Filament\Actions\ViewAction;
+use Filament\Tables\Table;
+use Filament\Tables\Columns\ImageColumn;
+use Filament\Tables\Columns\TextColumn;
+
+class ProductsTable
+{
+    public static function configure(Table $table): Table
+    {
+        return $table
+            ->columns([
+                //
+                TextColumn::make('name'),
+                TextColumn::make('sku'),
+                TextColumn::make('price'),
+                TextColumn::make('stock'),
+                ImageColumn::make('image')
+                    ->disk('public'),
+
+                TextColumn::make('is_active')
+                    ->label('Status')
+                    ->badge() 
+                    ->color(fn(string $state): string => $state ? 'success' : 'danger') 
+                    ->formatStateUsing(fn(string $state): string => $state ? 'Aktif' : 'Non-Aktif'), 
+            ])
+            ->filters([
+                //
+            ])
+            ->recordActions([
+                ViewAction::make(),
+                EditAction::make(),
+            ])
+            ->toolbarActions([
+                BulkActionGroup::make([
+                    DeleteBulkAction::make(),
+                ]),
+            ]);
+    }
+}
